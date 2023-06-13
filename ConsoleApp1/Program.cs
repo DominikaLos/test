4dc8f8571c26bb1a79cd056b88c55146eafe44ab
DominikaLos <dominika.los@arup.com>
2023-06-13 15:50:07 +0200

    sample code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!" + "abc");
+
+Console.WriteLine("want to test it~");
+
+List<string> list = new() {
+  "safsadf",
+  "asdfas",
+  "asdfasdf",
+  "asdfasdfasd",
+};
+
+var c = new Class1();
+c.Something("sdhasdk");

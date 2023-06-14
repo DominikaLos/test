b6c993fd816b24db2cc7a8da2a57515bbdb8b1cb
DominikaLos <83698580+DominikaLos@users.noreply.github.com>
2023-06-14 12:02:29 +0200

    Update Class1.cs

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -1,7 +1,10 @@
 ﻿namespace ConsoleApp1 {
   internal class Class1 {
-    public void Something(string a) {
-      Console.WriteLine(a);
-    }
+    public void Something(
+      string a) 
+    {
+      Console
+        .WriteLine(
+        a);    }
   }
 }
